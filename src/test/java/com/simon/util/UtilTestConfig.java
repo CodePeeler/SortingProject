355bae6b6571389d1c6252817674271b5a7e8f44
Simon <dynamowebdesign@gmail.com>
2017-12-31 19:32:18 +0000

    revert change as asked

--- src/test/java/com/simon/util/UtilTestConfig.java
+++ src/test/java/com/simon/util/UtilTestConfig.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
 public class UtilTestConfig {
-		
+	
+	@Profile("test")	
 	@Bean	 
 	public RandomDouble randomDouble() {
 		return new RandomDouble();
 	}
 	
+	@Profile("test")	
 	@Bean	 
 	public RandomInt randomInt() {
 		return new RandomInt(100);
