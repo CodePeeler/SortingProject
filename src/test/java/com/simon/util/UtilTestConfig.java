aa7618cc06b1ff3293bf979b479e4aac20de2acc
Simon <dynamowebdesign@gmail.com>
2017-12-31 19:06:35 +0000

    Cleaned-up HomeController, added some comments and cleaned up UtilTestConfig moved annotation

--- src/test/java/com/simon/util/UtilTestConfig.java
+++ src/test/java/com/simon/util/UtilTestConfig.java
@@ -5,16 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("test")
 public class UtilTestConfig {
 		
-	@Bean
-	@Profile("test") 
+	@Bean	 
 	public RandomDouble randomDouble() {
 		return new RandomDouble();
 	}
 	
-	@Bean
-	@Profile("test") 
+	@Bean	 
 	public RandomInt randomInt() {
 		return new RandomInt(100);
 	}
